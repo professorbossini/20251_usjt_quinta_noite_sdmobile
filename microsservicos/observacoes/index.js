00a819146ac4e6f8484457331fccdaad0ea7344f
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-17 21:31:58 -0300

    learn(ap01-mss): implementa endpoints
    - implementamos endpoints de observações e testamos com a thunder client
    - retomar estudando a figura 4.3.10 em detalhes
    - fazer a implementação do barramento a seguir

---
+++ microsservicos/observacoes/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const {v4: uuidv4 } = require('uuid')
+const app = express()
+app.use(express.json())
+
+/*
+{
+  1: [
+    {
+      id: 1000,
+      idLembrete: 1,
+      texto: 'comprar legumes'
+    },
+    {
+      id: 1001,
+      idLembrete: 1,
+      texto: 'comprar frutas'
+    }
+  ],
+}
+*/
+
+const baseObservacoes = {
+
+}
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+  //devolver apenas a coleção de observações do lembrete cujo id se encontra na url
+  //ou uma lista vazia caso ele ainda não possua observação alguma[]
+  res.json(baseObservacoes[req.params.id] || [])
+})
+
+///lembretes/1/observacoes
+app.post('/lembretes/:idLembrete/observacoes', (req, res) => {
+  const idObs = uuidv4()
+  const idLembrete = req.params.idLembrete
+  const { texto } = req.body
+  const observacao = {
+    id: idObs,
+    idLembrete: idLembrete,
+    texto: texto
+  }
+  const observacoes = baseObservacoes[idLembrete] || []
+  observacoes.push(observacao)
+  baseObservacoes[idLembrete] = observacoes
+  res.status(201).json(observacoes)
+})
+
+const port = 5000
+app.listen(port, () => {
+  console.log (`Observações. Porta ${port}.`)
+})
